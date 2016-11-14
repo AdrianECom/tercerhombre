1c8851e794d2e34cc9c00aa0d36d3ba2eef85ec8
Adrián Egea <adrianensis@gmail.com>
2016-11-14 11:01:20 +0100

    Corregida regla solo se ejecuta una vez.

--- src/main/java/tercerhombre/personaje/Personaje.java
+++ src/main/java/tercerhombre/personaje/Personaje.java
@@ -1,8 +1,6 @@
 package tercerhombre.personaje;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
 
 import tercerhombre.propiedades.Actividad;
