4286b26ff3547d3a9ec101ca97e981db7420a6ac
Jesus <Jesus@Dell>
2016-11-14 12:44:49 +0100

    LectorConsultas Beta

--- src/main/java/tercerhombre/LectorConsultas.java
+++ src/main/java/tercerhombre/LectorConsultas.java
@@ -8,80 +8,75 @@ import java.io.IOException;
 
 public class LectorConsultas {
 
-	 public static String muestraContenido(String archivo) throws FileNotFoundException, IOException {
-	        String linea = null;
-		 	String cadena;
-	        FileReader f = new FileReader(archivo);
-	        BufferedReader b = new BufferedReader(f);
-	        while((cadena = b.readLine())!=null) {
-	            linea = cadena;
-	        }
-	        b.close();
-			return linea;
-	    }
-	 
-private static void Que (String archivo) throws FileNotFoundException {
-	     
-		 Pattern patron = Pattern.compile("Qué");
-		 Matcher match = patron.matcher(archivo);
-		 boolean que = match.find();
-		 System.out.println(archivo);
+	public static void Consulta(String archivo) throws FileNotFoundException {
 
-		 Pattern acto = Pattern.compile("hasta ([Acto]+\\d{1})");
-		 Matcher matchActo = acto.matcher(archivo);
+		Pattern patronQue = Pattern.compile("Qué");
+		Matcher matchQue = patronQue.matcher(archivo);
+		Pattern patronQuien = Pattern.compile("Quién");
+		Matcher matchQuien = patronQuien.matcher(archivo);
 
-		 boolean matchActosi = matchActo.find();
-
-		 System.out.println("Ha encontrado que? "+que);
-		 System.out.println("Ha encontrado Acto? "+matchActosi);
-		 
-		 if (matchActosi){
-			 String a2 = matchActo.group(1);
-			 System.out.println(a2);
-		}		 else {
-			 System.out.println("No ha encontrado preguntas QUÉ");
-		 }
-		 
-	 }
-	 
-	 
-	 private static void Quien (String archivo) throws FileNotFoundException {
-	     
-		 Pattern patron = Pattern.compile("[Quién]");
-		 Matcher match = patron.matcher(archivo);
-		 boolean quien = match.find();
-		 System.out.println(archivo);
-		 Pattern persona = Pattern.compile("es (\\p{Lu}+\\p{Ll}*) hasta");
-		 Matcher matchPersona = persona.matcher(archivo);
-		 Pattern acto = Pattern.compile("hasta ([Acto]+\\d{1})");
-		 Matcher matchActo = acto.matcher(archivo);
-
-		 boolean matchPersonasi = matchPersona.find();
-		 boolean matchActosi = matchActo.find();
-
-		 System.out.println("Ha encontrado quien? "+quien);
-		 System.out.println("Ha encontrado Persona? "+matchPersonasi);
-		 System.out.println("Ha encontrado Acto? "+matchActosi);
-
-		 
-		 if (matchPersonasi){
-		 String el2 = matchPersona.group(1);
-		 System.out.println(el2);
-		 }
-		 
-		 if (matchActosi){
-			 String a2 = matchActo.group(1);
-			 System.out.println(a2);
+		if (matchQue.find()) {
+			System.out.println("QUE");
+			Que(archivo);
+		} else if (matchQuien.find()) {
+			System.out.println("QUIEN");
+			Quien(archivo);
 		}
-		 else {
-			 System.out.println("No ha encontrado preguntas QUIEN");
-		 }
-		 
-	 }
 
-	    public static void main(String[] args) throws IOException {
-	        String lector = muestraContenido("C:/Users/Jesus/Desktop/lector.csv");       
-	        Quien (lector);
-	        Que (lector);
-	    }
+	}
+
+	public static String muestraContenido(String archivo)
+			throws FileNotFoundException, IOException {
+		String linea = null;
+		String cadena;
+		FileReader f = new FileReader(archivo);
+		BufferedReader b = new BufferedReader(f);
+		while ((cadena = b.readLine()) != null) {
+			linea = cadena;
+		}
+		b.close();
+		return linea;
+	}
+
+	private static void Que(String archivo) throws FileNotFoundException {
+
+		Pattern acto = Pattern.compile("hasta ([Acto]+\\d{1})");
+		Matcher matchActo = acto.matcher(archivo);
+
+		boolean matchActosi = matchActo.find();
+
+		if (matchActosi) {
+			String a2 = matchActo.group(1);
+			System.out.println(a2);
+		}
+	}
+
+	private static void Quien(String archivo) throws FileNotFoundException {
+
+		System.out.println(archivo);
+		Pattern persona = Pattern.compile("es (\\p{Lu}+\\p{Ll}*) hasta");
+		Matcher matchPersona = persona.matcher(archivo);
+		Pattern acto = Pattern.compile("hasta ([Acto]+\\d{1})");
+		Matcher matchActo = acto.matcher(archivo);
+
+		boolean matchPersonasi = matchPersona.find();
+		boolean matchActosi = matchActo.find();
+
+		if (matchPersonasi) {
+			String el2 = matchPersona.group(1);
+			System.out.println(el2);
+		}
+
+		if (matchActosi) {
+			String a2 = matchActo.group(1);
+			System.out.println(a2);
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		String lector = muestraContenido("C:/Users/Jesus/Desktop/lector.csv");
+		Consulta(lector);
+
+	}
 }
